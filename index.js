dfb4fd73b4707c2f2899b12c4857b2d47279422c
Tyler <tylersayvetz@gmail.com>
2019-12-05 17:29:03 -0800

    here jordan

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-//yay
+const express = require('express');
+const app = express();
 
+app.listen(3000, () => {
+    console.log("The server is now running");
+})
+
+app.get('/', (req, res) => {
+    res.send("Root folder");
+})
